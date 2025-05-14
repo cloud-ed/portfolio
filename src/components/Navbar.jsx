f06c4107bc6a7018eab50d834919457059a863c4
Josh <f360f2@outlook.com>
2025-05-14 11:54:21 +1000

    feat: :sparkles: create basic navbar with MaterialUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,41 @@
-import * as React from "react";
-export default function Navbar() {
-    return(
-        <div style={{ width: "100%"}}></div>
-    )
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import HomeIcon from '@mui/icons-material/Home';
+
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="home"
+            sx={{ mr: 2 }}
+            onClick={topFunction}
+          >
+            <HomeIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            clouded
+          </Typography>
+          <Button color="inherit" href='#about'>About</Button>
+          <Button color="inherit" href='#projects'>Projects</Button>
+          <Button color="inherit" href='#skills'>Skills</Button>
+          <Button color="inherit" href='#contact'>Contact</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
