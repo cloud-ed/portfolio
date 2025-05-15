23417571dfff356293128fddfdba0f384e4e4537
Josh <f360f2@outlook.com>
2025-05-15 15:41:18 +1000

    style: :lipstick: wrap content in box to avoid being hidden by navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import { Box } from "@mui/material";
 
 export default function App() {
   return (
     <main>
       <Navbar />
-      <About />
-      {/* <Projects />
-      <Skills />
-      <Contact /> */}
+      <Box sx={{ pt: { xs: 7, sm: 8 } }}>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </Box>
     </main>
   );
 }
