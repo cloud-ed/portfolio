9679420b8f1f7500c206dd3dc709067cba4bd521
Josh <f360f2@outlook.com>
2025-05-15 17:24:43 +1000

    feat: :art: store projects and skills in seperate file for easy management

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const projects = [
+  {
+    title: "CloudedClicker",
+    subtitle: "Python",
+    description:
+      "A lightweight, customizeable autoclicker and macro tool for Windows. Built with Pynput and Tkinter",
+    image: "./logo192.png",
+    link: "https://github.com/cloud-ed/CloudedClicker",
+  },
+  {
+    title: "Portfolio Website",
+    subtitle: "JavaScript",
+    description:
+      "The website you're on right now! Built with React, TailwindCSS and MaterialUI. Deployed with Vercel",
+    image: "./logo192.png",
+    link: "https://github.com/cloud-ed/PortfolioWebsite",
+  },
+  {
+    title: "Missing Dog Search",
+    subtitle: "Python",
+    description:
+      "A search algorithmn that pulls the top 10 results for facebook posts that have relevant information to a specific missing dog. Built using Google API and Google CSE",
+    image: "./logo192.png",
+    link: "https://github.com/cloud-ed/MissingDogSearch",
+  },
+];
+
+export const skills = [
+  "Python",
+  "JavaScript",
+  "TypeScript",
+  "C#",
+  "React",
+  "Node.js",
+  "MySQL",
+  "MongoDB",
+  "HTML",
+  "CSS",
+];
