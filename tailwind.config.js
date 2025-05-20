a456a9520ec848493e4a67563730ed1c7a21a185
Josh <f360f2@outlook.com>
2025-05-20 15:42:30 +1000

    style: :lipstick: set custom margins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        custom: "1200px",
+        custom_nav: "1100px",
+      },
+    },
   },
   plugins: [],
 };
