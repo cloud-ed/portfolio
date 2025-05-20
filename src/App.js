e7f5217be14280a9f1ced6af965882daf2971a19
Josh <f360f2@outlook.com>
2025-05-20 15:47:14 +1000

    chore: :art: reformat code without MUI

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import { Box, Container } from "@mui/material";
 
 export default function App() {
   return (
-    <main className="text-black bg-white body-font">
-      <Container maxWidth="lg">
-        <Navbar />
-        <Box sx={{ pt: { xs: 7, sm: 8 } }}>
-          <About />
-          <Projects />
-          <Skills />
-          <Contact />
-        </Box>
-      </Container>
+    <main className="text-black bg-white body-font px-4 md:px-8 lg:px-12 max-w-custom mx-auto">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
     </main>
   );
 }
