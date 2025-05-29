184107838293cdf1c5e987a67f19f2ae13f34378
Josh <f360f2@outlook.com>
2025-05-29 15:24:36 +1000

    style: :lipstick: update project images

--- src/data.js
+++ src/data.js
@@ -4,24 +4,24 @@ export const projects = [
     subtitle: "Python",
     description:
       "A lightweight, customizeable autoclicker and macro tool for Windows. Built with Pynput and Tkinter",
-    image: "./logo192.png",
+    image: "./cloudedclicker.webp",
     link: "https://github.com/cloud-ed/CloudedClicker",
-  },
-  {
-    title: "Portfolio Website",
-    subtitle: "JavaScript",
-    description:
-      "The website you're on right now! Built with React and TailwindCSS. Deployed with Vercel",
-    image: "./logo192.png",
-    link: "https://github.com/cloud-ed/PortfolioWebsite",
   },
   {
     title: "Missing Dog Search",
     subtitle: "Python",
     description:
       "A search algorithmn that pulls the top 10 results for facebook posts that have relevant information to a specific missing dog. Built using Google API and Google CSE",
-    image: "./logo192.png",
+    image: "./missingdogsearch.webp",
     link: "https://github.com/cloud-ed/MissingDogSearch",
+  },
+  {
+    title: "Portfolio Website",
+    subtitle: "JavaScript",
+    description:
+      "The website you're on right now! Built with React and TailwindCSS. Deployed with Vercel",
+    image: "./portfolio.webp",
+    link: "https://github.com/cloud-ed/PortfolioWebsite",
   },
 ];
 
